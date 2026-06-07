46c5ae54f90e197336cec7e20786e09f371b106a
SukumarRenukuntla <sukumarrenukuntla@gmail.com>
2026-06-07 09:00:10 +0530

    Added  database schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE github_profiles (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) UNIQUE,
+    name VARCHAR(255),
+    followers INT,
+    following INT,
+    public_repos INT,
+    avatar_url TEXT,
+    profile_url TEXT,
+    analyzed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);gi
